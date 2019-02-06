3863ff8a6709b1b7cd6f023a371abec9b69df1ec
Ashley Wong <ashwong04@gmail.com>
2019-02-06 15:44:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       search_term = req.path.split("/").last
         binding.pry
-        if @@items.include?(search_term)
+        if @@items.find{search_term}
           resp.write "#{search_term.price}"
         else
           resp.write "Item not found"
